d8de7786a7aa51e79d759713bbefa4cbc7c6c6df
LaGuitza <laguitza@proton.me>
2025-04-04 17:22:01 +0200

    Primer commit

---
+++ src/js/color.js
@@ -0,0 +1,16 @@
+function changeColor(newColor) {
+    const elem = document.getElementById("canvi");
+    elem.style.backgroundColor = newColor;
+    const elem2 = document.getElementById("canvi2");
+    elem2.style.backgroundColor = newColor;
+    const elem3 = document.getElementById("canvi3");
+    elem3.style.backgroundColor = newColor;
+    const elem4 = document.getElementById("canvi4");
+    elem4.style.backgroundColor = newColor;
+    const elem5 = document.getElementById("canvi5");
+    elem5.style.backgroundColor = newColor;
+    const elem6 = document.getElementById("canvi6");
+    elem6.style.backgroundColor = newColor;
+    const elem7 = document.getElementById("canvi7");
+    elem7.style.backgroundColor = newColor;
+  }
